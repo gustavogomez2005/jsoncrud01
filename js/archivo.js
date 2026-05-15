badf41b9ae93d0ccbe9a92db398882fa53d332b2
gustavogomez2005 <gustavogomez2005@gmail.com>
2026-05-14 21:15:02 -0300

    filtrado terminado

--- js/archivo.js
+++ js/archivo.js
@@ -14,9 +14,20 @@ document.getElementById('btnFiltrar').addEventListener('click', ()=>{
         })
         .then((libros)=>{
             console.log(libros);
+            console.log(autorBuscado);
+            let librosDelAutor = libros.filter( (libro)=> libro.autor == autorBuscado);
+            console.log(librosDelAutor);
+            let divmuestra = document.getElementById('muestra');
+            divmuestra.innerHTML = "";            
+            librosDelAutor.forEach(libro =>{
+                let li = document.createElement('li');
+                li.textContent = `Título: ${libro.titulo} Autor: ${libro.autor}`
+                divmuestra.appendChild(li);                
+            })
         })
         .catch( (e)=>{
-            alert(`Autor inexistente\n ${e}`)
+           // alert(`Autor inexistente\n ${e}`)
+           console.log('error');
         })
 })
 
