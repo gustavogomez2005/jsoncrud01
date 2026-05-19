fed182dd93291ce21712e7f6c83e86a62427153c
gustavogomez2005 <gustavogomez2005@gmail.com>
2026-05-19 09:37:29 -0300

    modificzando el metodo de alta

--- js/alta.js
+++ js/alta.js
@@ -9,25 +9,25 @@ document.getElementById('btnAlta').addEventListener('click', ()=>{
         })
         .then( (libros)=>{
 
-    let id, tit, nom, precio, stk, dispo, edit;
-    id = document.getElementById('txtid').value;
-    tit = document.getElementById('txttitulo').value;
-    nom = document.getElementById('txtautor').value;
-    precio = document.getElementById('txtprecio').value;
-    stk = document.getElementById('txtstock').value;
-    dispo = document.getElementById('chkdisponible').value;
-    edit = document.getElementById('txteditorial').value;
+            let id, tit, nom, precio, stk, dispo, edit;
+            id = document.getElementById('txtid').value;
+            tit = document.getElementById('txttitulo').value;
+            nom = document.getElementById('txtautor').value;
+            precio = document.getElementById('txtprecio').value;
+            stk = document.getElementById('txtstock').value;
+            dispo = document.getElementById('chkdisponible').value;
+            edit = document.getElementById('txteditorial').value;
 
-    let nuevoLibro = {
-        id: id,
-        titulo: tit,
-        autor: nom,
-        precio: precio,
-        editorial: edit,
-        stock: stk,
-        disponible: dispo
-    };
-console.log(nuevoLibro);
+            let nuevoLibro = {
+                id: id,
+                titulo: tit,
+                autor: nom,
+                precio: precio,
+                editorial: edit,
+                stock: stk,
+                disponible: dispo
+            };
+            console.log(nuevoLibro);
 /*            let objetosLibros = JSON.parse(libros);
             console.log(objetosLibros);
              console.log(typeof(objetosLibros));
