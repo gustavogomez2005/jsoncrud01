1da9d0d7e6317ab2610c8ef3f5f1098e182f0945
gustavogomez2005 <gustavogomez2005@gmail.com>
2026-05-19 09:55:42 -0300

    terminado el alta

--- js/alta.js
+++ js/alta.js
@@ -2,20 +2,18 @@ let archivo = './json/libros.json';
 
 
 document.getElementById('btnAlta').addEventListener('click', ()=>{
-
-    fetch(archivo)
-        .then( (rpta)=>{
-            return rpta.json();
-        })
-        .then( (libros)=>{
-
             let id, tit, nom, precio, stk, dispo, edit;
-            id = document.getElementById('txtid').value;
+            id = parseInt(document.getElementById('txtid').value);
             tit = document.getElementById('txttitulo').value;
             nom = document.getElementById('txtautor').value;
-            precio = document.getElementById('txtprecio').value;
-            stk = document.getElementById('txtstock').value;
+            precio = parseInt(document.getElementById('txtprecio').value);
+            stk = parseInt(document.getElementById('txtstock').value);
             dispo = document.getElementById('chkdisponible').value;
+            if (dispo == "on") {
+                dispo = true;
+            } else {
+                dispo = false;
+            }
             edit = document.getElementById('txteditorial').value;
 
             let nuevoLibro = {
@@ -28,17 +26,29 @@ document.getElementById('btnAlta').addEventListener('click', ()=>{
                 disponible: dispo
             };
             console.log(nuevoLibro);
-/*            let objetosLibros = JSON.parse(libros);
+
+        fetch(archivo)
+        .then( (rpta)=>{
+            return rpta.json();
+        })
+        .then( (libros)=>{
+            console.log(libros);
+            console.log(typeof(libros));
+            libros.push(nuevoLibro);            
+            console.log(libros);
+        })
+        .catch( (e)=>{
+            alert(`Error al leer el json de Libros\n ${e}`)
+        })
+
+    })
+
+/*
+                let objetosLibros = JSON.parse(libros);
             console.log(objetosLibros);
              console.log(typeof(objetosLibros));
             objetosLibros.push(nuevoLibro);
             let librosact = JSON.stringify(objetosLibros);
             console.log(typeof(librosact));
-            console.log(librosact); */
-console.log(libros);
-console.log(typeof(libros));
-        })
-        .catch( (e)=>{
-            alert(`Error al leer el json de Libros\n ${e}`)
-        })
-})
+            console.log(librosact); 
+*/
